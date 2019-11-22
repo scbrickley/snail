b151db32850bf003f7a245b07b5f1677ba56cd1b
Sean Brickley <sean@wabr.io>
2019-11-22 17:19:20 -0500

    Tested for matrices with even dimensions

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,11 @@ fn snail(mut matrix: Vec<Vec<u32>>) -> Vec<u32> {
             .map(|list| list.pop().unwrap())
             .collect());
 
-        let mut last_row: Vec<u32> = matrix.pop().unwrap();
+        let mut last_row = matrix.pop().unwrap();
         last_row.reverse();
         output.append(&mut last_row);
+
+        if matrix.is_empty() { break }
 
         let mut left_column: Vec<u32> = matrix.iter_mut()
             .map(|list| list.remove(0))
@@ -54,5 +56,16 @@ fn test_snail() {
                       vec![ 7, 8, 9 ]];
 
     let expected_output = vec![ 1, 2, 3, 6, 9, 8, 7, 4, 5 ];
+    assert_eq!(snail(matrix), expected_output);
+}
+
+#[test]
+fn test_snail_even_length_matrix() {
+    let matrix = vec![vec![  1,  2,  3,  4 ],
+                      vec![  5,  6,  7,  8 ],
+                      vec![  9, 10, 11, 12 ],
+                      vec![ 13, 14, 15, 16 ]];
+
+    let expected_output = vec![ 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10];
     assert_eq!(snail(matrix), expected_output);
 }
