b43b58e30e0ea7702919b5681ae750099160fa6e
Sean Brickley <sean@wabr.io>
2020-01-22 11:45:51 -0500

    Minor readability fix to vec! declarations in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn test_snail() {
     let matrix = vec![
 			vec![1, 2, 3],
 			vec![4, 5, 6],
-			vec![7, 8, 9]
+			vec![7, 8, 9],
 		];
 
     let expected_output = vec![1, 2, 3, 6, 9, 8, 7, 4, 5];
